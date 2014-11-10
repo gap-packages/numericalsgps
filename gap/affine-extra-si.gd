890b1907c954596a9f41616f52f90d6b77eb57b9
Pedro A. Garcia-Sanchez <pedro@ugr.es>
2014-11-10 12:14:09 +0100

    Alternative method for minimal presentations, betti elements and catenary degree for affine semigroups not depending on SingularInterface Alternative method for the above also in singular

--- gap/affine-extra-si.gd
+++ gap/affine-extra-si.gd
@@ -2,17 +2,20 @@
 #F BettiElementsOfAffineSemigroup
 # Computes the Betti elements of the affine semigroup a
 ###########################################################################
-DeclareGlobalFunction("BettiElementsOfAffineSemigroup");
+#DeclareGlobalFunction("BettiElementsOfAffineSemigroup");
+#Moved to affine.gd
 
 ###############################################################################
 #F MinimalPresentationOfAffineSemigroup
 # Computes a minimal presentation of the affine semigroup a
 ###########################################################################
-DeclareGlobalFunction("MinimalPresentationOfAffineSemigroup");
+#DeclareGlobalFunction("MinimalPresentationOfAffineSemigroup");
+#Moved to affine.gd
 
 ###############################################################################
 #F CatenaryDegreeOfAffineSemigroup
 # Computes the catenary degree of the affine semigroup a
 ###########################################################################
-DeclareGlobalFunction("CatenaryDegreeOfAffineSemigroup");
+#DeclareGlobalFunction("CatenaryDegreeOfAffineSemigroup");
+#Moved to affine.gd
 
