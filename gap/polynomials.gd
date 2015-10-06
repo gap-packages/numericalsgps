e359c3a2c9414876410c9a7f08a8498a087075ca
Pedro A Garcia-Sanchez <pedro@ugr.es>
2015-10-06 12:14:34 +0200

    Added IsNumericalSemigroupPolynomial and NumericalSemigroupFromNumericalSemigroupPolynomial with documentation

--- gap/polynomials.gd
+++ gap/polynomials.gd
@@ -1,22 +1,39 @@
 #############################################################################
 ##
-#W  arf-med.gi              Manuel Delgado <mdelgado@fc.up.pt>
+#W  polynomials.gi              Manuel Delgado <mdelgado@fc.up.pt>
 #W                          Pedro A. Garcia-Sanchez <pedro@ugr.es>
 ##
 ##
-#Y  Copyright 2014 by Manuel Delgado and Pedro Garcia-Sanchez 
-#Y  We adopt the copyright regulations of GAP as detailed in the 
+#Y  Copyright 2015 by Manuel Delgado and Pedro Garcia-Sanchez
+#Y  We adopt the copyright regulations of GAP as detailed in the
 #Y  copyright notice in the GAP manual.
 ##
 #############################################################################
 #################################################
-## 
+##
 #F NumericalSemigroupPolynomial(s,x)
 ## s is a numerical semigroup, and x a variable (or a value)
 ## returns the polynomial (1-x)\sum_{s\in S}x^s
 ##
 ##################################################
 DeclareGlobalFunction("NumericalSemigroupPolynomial");
+
+##############################################################
+##
+#F IsNumericalSemigroupPolynomial(f) detects
+## if there exists S a numerical semigroup such that P_S(x)=f
+##
+##############################################################
+DeclareGlobalFunction("IsNumericalSemigroupPolynomial");
+
+##############################################################
+##
+#F NumericalSemigroupFromNumericalSemigroupPolynomial(f) outputs
+## a numerical semigroup S such that P_S(x)=f; error if no such
+## S exists
+##
+##############################################################
+DeclareGlobalFunction("NumericalSemigroupFromNumericalSemigroupPolynomial");
 
 ###################################################
 #F HilbertSeriesOfNumericalSemigroup(s,x)
@@ -34,17 +51,17 @@ DeclareGlobalFunction("GraeffePolynomial");
 
 #####################################################
 ##
-#F IsCyclotomicPolynomial(f) detects 
-## if f is a cyclotomic polynomial using the method explained in 
+#F IsCyclotomicPolynomial(f) detects
+## if f is a cyclotomic polynomial using the method explained in
 ## BD-cyclotomic
 #####################################################
 DeclareGlobalFunction("IsCyclotomicPolynomial");
 
 ########################################################################
 ##
-#F IsKroneckerPolynomial(f) decides if 
-##   f is a Kronecker polynomial, that is,   a monic polynomial with integer coefficients 
-##   having all its roots in the unit circunference, equivalently, is a product of 
+#F IsKroneckerPolynomial(f) decides if
+##   f is a Kronecker polynomial, that is,   a monic polynomial with integer coefficients
+##   having all its roots in the unit circunference, equivalently, is a product of
 ##   cyclotomic polynomials
 #########################################################################
 DeclareGlobalFunction("IsKroneckerPolynomial");
@@ -57,7 +74,7 @@ DeclareGlobalFunction("IsKroneckerPolynomial");
 DeclareGlobalFunction("IsCyclotomicNumericalSemigroup");
 
 #####################################################
-## 
+##
 #F IsSelfReciprocalUnivariatePolynomial(p)
 ## Checks if the univariate polynomial p is selfreciprocal
 #####################################################
@@ -66,18 +83,18 @@ DeclareGlobalFunction("IsSelfReciprocalUnivariatePolynomial");
 #################################################################
 ##
 # F SemigroupOfValuesOfPlaneCurveWithSinglePlaceAtInfinity(f)
-##  Computes the semigroup of values {mult(f,g) | g curve} of a plane curve 
+##  Computes the semigroup of values {mult(f,g) | g curve} of a plane curve
 ##   with one place at the infinity in the variables X(Rationals,1) and X(Rationals,2)
 ##  f must be monic on X(Rationals(2))
 ##  SemigroupOfValuesOfPlaneCurveWithSinglePlaceAtInfinity(f,"all")
-##    The same as above, but the output are the approximate roots and  
+##    The same as above, but the output are the approximate roots and
 ##    delta-sequence
 ##
 #################################################################
 DeclareGlobalFunction("SemigroupOfValuesOfPlaneCurveWithSinglePlaceAtInfinity");
 
 #########################################################################
-## 
+##
 #F IsDeltaSequence(l)
 ## tests whether or not l is a \delta-sequence (see for instancd [AGS14])
 ##
@@ -95,7 +112,7 @@ DeclareGlobalFunction("DeltaSequencesWithFrobeniusNumber");
 #####################################################
 ##
 #F CurveAssociatedToDeltaSequence(l)
-##  computes the curve associated to a delta-sequence l in 
+##  computes the curve associated to a delta-sequence l in
 ##  the variables X(Rationals,1) and X(Rationals,2)
 ##  as explained in [AGS14]
 ##
