9bbbe49b1d23db823fcdbb9dfe8445ec389236ef
Pedro A Garcia-Sanchez <pedro@ugr.es>
2016-09-01 18:49:34 +0100

    Added HolesOfNumericalSemigroup

--- gap/basics2.gd
+++ gap/basics2.gd
@@ -5,9 +5,9 @@
 #W                          Jose Morais <josejoao@fc.up.pt>
 ##
 ##
-#Y  Copyright 2005 by Manuel Delgado, 
+#Y  Copyright 2005 by Manuel Delgado,
 #Y  Pedro Garcia-Sanchez and Jose Joao Morais
-#Y  We adopt the copyright regulations of GAP as detailed in the 
+#Y  We adopt the copyright regulations of GAP as detailed in the
 #Y  copyright notice in the GAP manual.
 ##
 #############################################################################
@@ -15,9 +15,9 @@
 #############################################################################
 ##
 #O  IsSubsemigroupOfNumericalSemigroup(S,T)
-#O  IsSubset (is a synonym) 
-## 
-##  Test whether the numerical semigroup T is contained in the 
+#O  IsSubset (is a synonym)
+##
+##  Test whether the numerical semigroup T is contained in the
 ##  numerical semigroup S
 ##
 #############################################################################
@@ -29,7 +29,7 @@ DeclareOperation( "IsSubsemigroupOfNumericalSemigroup",[IsNumericalSemigroup,IsN
 #############################################################################
 ##
 #F  IntersectionOfNumericalSemigroups(S,T)
-## 
+##
 ##  Returns the intersection of the numerical
 ##  semigroups S and T.
 ##
@@ -55,7 +55,7 @@ DeclareGlobalFunction("RepresentsGapsOfNumericalSemigroup");
 #F  NumericalSemigroupsWithFrobeniusNumber(g)
 ##
 ##  Computes the set of numerical semigroups with Frobenius number g.
-##  The algorithm is based on 
+##  The algorithm is based on
 ##  "Fundamental gaps in numerical semigroup".
 ##
 #############################################################################
@@ -63,27 +63,27 @@ DeclareGlobalFunction("NumericalSemigroupsWithFrobeniusNumber");
 
 ##############################################################################
 ##
-#F NumericalSemigroupsWithGenus 
+#F NumericalSemigroupsWithGenus
 ##computes the set of numerical semigroups with genus g,
 # that is, numerical semigroups with exactly g gaps
 #
 #
 # numerical semigroups are encoded in lists containing the apery set with
 # respect to the multiplicity removing the zero element. The multiplicity
-# is thus the lenght of the list plus one. In this way deciding membership 
+# is thus the lenght of the list plus one. In this way deciding membership
 # to a numerical semigroup is straightforward (belongs). The computation of
 # the Frobenius number is performed using Selmer's idea (frob). Removing a new
-# generator is easy (removegen), as well as computing those minimal generators 
-# greater than the Frobenius number (minimalgeneratorsf). 
-# Given a numerical semigroup of genus g, removing minimal generators, one 
+# generator is easy (removegen), as well as computing those minimal generators
+# greater than the Frobenius number (minimalgeneratorsf).
+# Given a numerical semigroup of genus g, removing minimal generators, one
 # obtains numerical semigroups of genus g+1. In order to avoid repetitions,
-# we only remove minimal generators greater than the frobenius number of 
+# we only remove minimal generators greater than the frobenius number of
 # the numerical semigroup (this is accomplished with the local function sons).
-# References: 
-# -J. C. Rosales, P. A. García-Sánchez, J. I. García-García and 
-#  J. A. Jimenez-Madrid, The oversemigroups of a numerical semigroup. 
+# References:
+# -J. C. Rosales, P. A. GarcÃ­a-SÃ¡nchez, J. I. GarcÃ­a-GarcÃ­a and 
+#  J. A. Jimenez-Madrid, The oversemigroups of a numerical semigroup.
 #  Semigroup Forum 67 (2003), 145--158.
-# -M. Bras-Amorós, Fibonacci-like behavior of the number of numerical 
+# -M. Bras-AmorÃ³s, Fibonacci-like behavior of the number of numerical
 #  semigroups of a given genus. Semigroup Forum 76 (2008), 379--384.
 ##
 #############################################################################
